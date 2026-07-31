1f015848513bf0b023542e5983a55f6c6187a341
Thainá Kr <thatafkr@gmail.com>
2026-07-31 10:26:21 -0300

    style: carrega tema responsivo do funil

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./prospecTheme.css";
+import "./funnelTheme.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
