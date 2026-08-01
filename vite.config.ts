47e6898e587b96da24985feb439d6ea89ea3b7ba
Thainá Kr <thatafkr@gmail.com>
2026-08-01 14:43:42 -0300

    fix: use root base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   plugins: [react(), viteSingleFile()],
-  base: "/prospec-kr/",
+  base: "/",
   build: {
     target: "es2022",
     sourcemap: false,
